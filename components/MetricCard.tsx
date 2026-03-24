a0e682725c1605fa924e711152c48f8961b738e1
lupelab <adlens@lupe.com.py>
2026-03-24 20:36:01 -0300

    Delete components directory

--- components/MetricCard.tsx
+++
@@ -1,9 +0,0 @@
-export function MetricCard({ title, value, detail }: { title: string; value: string | number; detail?: string }) {
-  return (
-    <div className="panel">
-      <div className="card-title">{title}</div>
-      <div className="metric">{value}</div>
-      {detail ? <div className="small" style={{ marginTop: 8 }}>{detail}</div> : null}
-    </div>
-  );
-}
